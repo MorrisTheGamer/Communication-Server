2d70f5da7e5ce56a27461e86ff4b927985e6433d
MorrisTheGamer <thore.dunker@gmx.de>
2026-02-11 09:23:01 +0100

    Implement Discord bot message forwarding

---
+++ server.js
@@ -0,0 +1,30 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const CHANNEL_IDS = [
+  "1470717206705868851",
+  "1470255943328596058",
+  "1425189805352030300"
+];
+
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+  if (!CHANNEL_IDS.includes(msg.channel.id)) return;
+
+  for (const id of CHANNEL_IDS) {
+    if (id === msg.channel.id) continue;
+    const channel = await client.channels.fetch(id);
+    if (channel) {
+      channel.send(`**${msg.author.username}**: ${msg.content}`);
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
